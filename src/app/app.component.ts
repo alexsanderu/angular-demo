79e86072faf1cbda7ac5a3d6a33d3fefd513b494
Alex Sander <sralexsadder@gmail.com>
2023-07-10 15:20:47 -0300

    Reformulação do html do site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.models';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularTest';
+  public todos: Todo[] = [];
+  public titule: String = 'My daily tasks'
+  public form: FormGroup;
+  
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(50),
+        Validators.required
+      ])]
+    })
+    this.todos.push(new Todo(1, 'hug a boy', false));
+    this.todos.push(new Todo(2, 'date with him', false));
+    this.todos.push(new Todo(3, 'cook for him', true));
+  }
+ 
+  otherTitle() {
+    this.titule = 'Completed'
+  }
+
+  remove(todo: Todo) {
+      const index = this.todos.indexOf(todo);
+      if(index !== -1) {
+          this.todos.splice(index, 1);
+      }
+  }
+
+  markAsDone(todo: Todo) {
+      todo.done=true;
+  }
+
+  markAsUndone(todo: Todo) {
+      todo.done=false;
+  }
 }
+
